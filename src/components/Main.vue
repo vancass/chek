2cd67ffed708d96a49ff3befaf1da62b54db6db8
Van <vanessa.cassandra.s@gmail.com>
2018-11-16 08:21:45 +0800

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="main">
+  <div class="centerer panel">
+  </div>
+  <div class="centerer panel">
+  </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.panel {
+  float: left;
+  width: 50%;
+  z-index: 10;
+  height: 100%;
+  position: relative;
+  text-align: center;
+  padding: 92px 80px;
+}
+.centerer {
+  max-width: 100%;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  text-align: center;
+}
+</style>
